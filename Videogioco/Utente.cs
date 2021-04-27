c6e6cac07f55dd1bd1c57c7c2730abf66bf39cec
patrickSbrighi <patrick.sbrighi00@gmail.com>
2021-04-27 08:44:16 +0200

    Aggiunta classe Utente di Simone Gramellini

--- Videogioco/Utente.cs
+++ Videogioco/Utente.cs
@@ -12,15 +12,32 @@ namespace Videogioco
     }
     public class Utente
     {
-        public Utente()
+        public Utente(Queue<Arma> armi, int puntiAttacco, string sourceArma, string sourcePersonaggio, bool schivato = false, Squadre squadra, int vitaUtente)
         {
-            
+            Armi = armi;
+            PuntiAttacco = puntiAttacco;
+            SourceArma = sourceArma;
+            SourcePersonaggio = sourcePersonaggio;
+            Schivato = schivato;
+            Squadra = squadra;
+            VitaUtente = vitaUtente;
         }
 
+        private Squadre _squadra;
         public Squadre Squadra
         {
-            get;
-            set;
+            get => default;
+            set
+            {
+                if (value == Squadre.Rossa || value == Squadre.Blu)
+                {
+                    _squadra = value;
+                }
+                else
+                {
+                    throw new Exception("Errore inserimento squadra utente");
+                }
+            }
         }
 
         public Personaggio Personaggio
@@ -37,57 +54,73 @@ namespace Videogioco
 
         public string SourcePersonaggio
         {
-            get => default;
-            private set
-            {
-            }
+            get;
+            private set;
         }
 
-        public int SourceArma
+        private string _sourceArma;
+        public string SourceArma
         {
-            get => default;
-            private set
-            {
-            }
+            get;
+            private set;
         }
 
         public bool Schivato
         {
-            get => default;
-            set
-            {
-            }
+            get;
+            set;
         }
 
+        private int _vitaUtente;
         public int VitaUtente
         {
             get => default;
             set
             {
+                if (value >= 0)
+                {
+                    _vitaUtente = value;
+                }
+                else
+                {
+                    throw new Exception("Errore inserimento vita utente");
+                }
             }
         }
 
+        private int _puntiAttacco;
         public int PuntiAttacco
         {
-            get => default;
+            get => _puntiAttacco;
             set
             {
+                if (value >= 0)
+                {
+                    _puntiAttacco = value;
+                }
+                else
+                {
+                    throw new Exception("Errore inserimento attacco utente");
+                }
             }
         }
 
-        public void AggiungiPuntiEsperienza(int punti)
+        private Personaggio _personaggio;
+        public void AggiungiPuntiEsperienza(int puntiEsperienzaDaAggiungere)
         {
-            throw new System.NotImplementedException();
+            _personaggio.Esperienza += puntiEsperienzaDaAggiungere;
         }
 
+        private Queue<Arma> _armi;
         public void CambioArma()
         {
-            throw new System.NotImplementedException();
+            _armi.Dequeue();
         }
 
         public void CalcoloSource()
         {
             throw new System.NotImplementedException();
+            //TODO
         }
     }
 }
